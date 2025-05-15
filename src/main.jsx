103bd963600e1cf92ba5ab774ce2f6da01ca31a9
Arvin-Leung <leungarvin@gmail.com>
2025-05-15 13:05:41 -0400

    revert back to hash routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Routes, Route, HashRouter, BrowserRouter } from 'react-router'
+import { Routes, Route, HashRouter } from 'react-router'
 import { Case1 } from './pages/Case1.jsx'
 import { Case2 } from './pages/Case2.jsx'
 import { Case3 } from './pages/Case3.jsx'
@@ -11,15 +11,15 @@ import { NotFound } from './pages/NotFound.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="*" element={ <NotFound/>}/>
-        <Route path="/seg_3125_assignment_1_portfolio/" element= {<App />} />
+        <Route path="/*" element={ <NotFound/>}/>
+        <Route path="/" element= {<App />} />
         <Route path="/case1" element= {<Case1/>}/>
         <Route path="/case2" element= {<Case2/>}/>
         <Route path="/case3" element= {<Case3/>}/>
         <Route path="/case4" element= {<Case4/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
